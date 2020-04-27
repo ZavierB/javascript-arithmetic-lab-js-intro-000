fa066c43845638641995ff38fd7422e6e0354170
Zavier Berolo <zavier.berolo@gmail.com>
2020-04-27 07:07:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function add(a, b){
+function add(a,b){
   return a + b
 }
 
@@ -10,23 +10,22 @@ function multiply(a, b){
   return a * b
 }
 
-function divide(a, b){
+function divide(a,b){
   return a / b
 }
 
 function increment(n){
-  return ++n
+  return(n + 1)
 }
 
 function decrement(n){
-  return --n
+  return(n - 1)
 }
 
+function makeInt(n){
+  return parseInt(n, 10)
+  }
 
-function makeInt(string){
-  return parseInt(string, 10)
-}
-
-function preserveDecimal(string){
-  return parseFloat(string)
+function preserveDecimal(n){
+  return parseFloat(n)
 }
